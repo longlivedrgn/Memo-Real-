df329f4c6ad62a31b2f803d6518626b3f509c460
longlivedrgn <kimye9808@naver.com>
2022-08-18 00:16:24 +0900

    메모 보기 화면 1

---
+++ Memo/DetailViewController.swift
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.swift
+//  Memo
+//
+//  Created by 김용재 on 2022/08/18.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
